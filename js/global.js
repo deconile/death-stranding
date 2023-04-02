e9f836167c30953772f1f043fb84607c38f4f013
Raymond <raymondnhim89@gmail.com>
2023-04-02 15:21:14 -0700

    added button and logo with new navbar with jquery

--- js/global.js
+++ js/global.js
@@ -1,17 +1,33 @@
 
 
 //RUN ON DOCUMENT READY
-$(document).on('ready',function(){
-
+$(document).ready(function(){
+    navBar();
 });
 
 
 //RUN ON PAGE LOADED
 $(window).on('load',function(){
-    
+
 });
 
 
+function navBar() {
+    //WRITE HTML / TEMPALTES
+    $('body').prepend(`<nav></nav>`);
+    $('nav').prepend(`<div class="logo"><img src="images/logos/logo_kojima.png"></div>`);
+    $('nav').append(`<ul></ul>`);
 
+    let titles = ['Home','About','Buy Now','Media','Blog','Community'];
+    let urls = ['index.html','about.html','buy-now.html','media.html','blog.html','community.html'];
+    
+    for(i = 0; i < titles.length; i++){
+        let temp = `<li><a href="${urls[i]}">${titles[i]}</a></li>`
+        $('nav').find('ul').append(temp);
+    }
 
+    //ADD FIND LOCATION URL
+    //SET TO ACTIVE
+
+}
 
