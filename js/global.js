845e27e5b1dc1edb633e5614f7e1c969dfaeaef3
Raymond <raymondnhim89@gmail.com>
2023-04-03 14:48:59 -0700

    Fixed the navbar to have active pages when clicked

--- js/global.js
+++ js/global.js
@@ -22,12 +22,19 @@ function navBar() {
     let urls = ['index.html','about.html','buy-now.html','media.html','blog.html','community.html'];
     
     for(i = 0; i < titles.length; i++){
-        let temp = `<li><a href="${urls[i]}">${titles[i]}</a></li>`
+        let temp = `<li><a href="${urls[i]}" class="navLinks">${titles[i]}</a></li>`
         $('nav').find('ul').append(temp);
     }
 
-    //ADD FIND LOCATION URL
-    //SET TO ACTIVE
+    // Grab path name through current location on the document
+    const activePage = window.location.pathname;
+    // Using a const var to query select all anchors in nav and loop through each one
+    const navLinks = document.querySelectorAll('nav a').forEach(link => {
+        // Check to see if the link href has the activePage
+        if(link.href.includes(`${activePage}`)){
+            // Add the active class if the statement is true
+            link.classList.add('active');
+        }
+    })
 
-}
-
+};
