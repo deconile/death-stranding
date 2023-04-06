dbd46b8b0dc58423a6ef99a752fb8f1b315fca63
deconile <deconile@gmail.com>
2023-04-05 19:14:07 -0700

    commit for pull

--- js/global.js
+++ js/global.js
@@ -2,13 +2,13 @@
 
 //RUN ON DOCUMENT READY
 $(document).on('ready',function(){
-
+    console.log('works');
 });
 
 
 //RUN ON PAGE LOADED
 $(window).on('load',function(){
-    
+    console.log('works');
 });
 
 
