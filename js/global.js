ffc59a3613b5e1be5d077a9a8400e653b7878b09
deconile <deconile@gmail.com>
2023-04-09 16:41:26 -0700

    added dynamic footer and image adjustments

--- js/global.js
+++ js/global.js
@@ -3,11 +3,23 @@
 //RUN ON DOCUMENT READY
 $(document).ready(function(){
     navBar();
+    footer();
 });
-
 
 //RUN ON PAGE LOADED
 $(window).on('load',function(){
+    voidBlankLinks();
+});
+
+//RUN WHEN WINDOW IS RESIZED
+$(window).on('resize',function(){
+
+});
+
+//RUN WHEN PAGE IS SCROLLED
+//THIS INCLUDES SCROLLING OF ANY KIND (NOT JUST USER CONTROLLED)
+$(window).on('scroll',function(){
+    
 });
 
 
@@ -36,3 +48,33 @@ function navBar() {
         }
     });
 };
+
+function footer(){
+    const template = `
+    <footer>
+        <div class="limited">
+            <div class="logos">
+                <div><img src="images/logos/steam-white.png" /></div>
+                <div><img src="images/logos/logo_epic.png" /></div>
+                <div><img src="images/logos/logo_kojima.png" /></div>
+                <div><img src="images/logos/logo-505-games.png" /></div>
+            </div>
+            <p class="legal">&copy;2022 Sony Interactive Entertainment Inc. / KOJIMA PRODUCTIONS Co., Ltd. / HIDEO KOJIMA. PC version published by 505 Games. 505 Games and the 505 Games logo are trademarks or registered trademarks of 505 Games SpA or its affiliates in the U.S. and/or other countries. DEATH STRANDING is a trademark of Sony Interactive Entertainment LLC.</p>
+            <div class="rating">
+                <img src="images/logos/rating.jpg" />
+            </div>
+        </div>
+    </footer>
+    `
+    $('body').append(template);
+}
+
+
+//SETS CLICK OVERRIDE TO PREVENT PAGE RELOAD ON BLANK LINKS
+function voidBlankLinks(){
+    $('a').each(function(){
+        if($(this).attr('href') === ''){
+            $(this).attr('href','javascript:void();')
+        }
+    });
+}
